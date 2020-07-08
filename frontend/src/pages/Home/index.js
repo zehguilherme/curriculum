308a9ddd3857a5cb4f59d9da138873615249626f
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-07-08 16:16:33 -0300

    Create Home page with initial styles

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import MainInformations from '../../components/MainInformations'
+import Education from '../../components/Education'
+import ProfessionalExperience from '../../components/ProfessionalExperience'
+import Knowledge from '../../components/Knowledge'
+import QualificationsOthers from '../../components/QualificationsOthers'
+import Footer from '../../components/Footer'
+
+import './styles.css'
+
+export default function Home () {
+  return (
+    <div className="home-container">
+      <div className="curriculum-container">
+        <MainInformations />
+
+        <Education />
+
+        <ProfessionalExperience />
+
+        <div className="knowledge-qualifications">
+          <Knowledge />
+
+          <QualificationsOthers />
+        </div>
+
+        <Footer />
+
+      </div>
+    </div>
+  )
+}
