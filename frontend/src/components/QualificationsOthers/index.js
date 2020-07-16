b6c10bd0ed88ab08a37ccc97da9a624956fabd65
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-07-16 15:43:02 -0300

    Add QualificationsOthers component

---
+++ frontend/src/components/QualificationsOthers/index.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+import { StylesQualificationsOthers } from './styles'
+
+const ContainerQualificationsOthers = () => {
+  return (
+    <StylesQualificationsOthers>
+      <h2>Qualificações e outras atividades</h2>
+
+      <ul>
+        <li>
+          <h3>Next Level Week 1 pela Rocketseat – Criação da aplicação “Ecoleta”</h3>
+          <p>01/06/20 à 07/06/20 – 10h.</p>
+        </li>
+
+        <li>
+          <h3>11ª Semana Omnistack pela Rocketseat – Criação da aplicação “Be The Hero”</h3>
+          <p>23/03/20 à 29/03/20 – 6h.</p>
+        </li>
+
+        <li>
+          <h3>3ª Maratona-Dev pela Rocketseat – Criação de aplicação web para cadastro de doadores de sangue</h3>
+          <p>17/02/20 à 19/02/20 – 4h.</p>
+        </li>
+
+        <li>
+          <h3>2º Bootcamp pela Ez.devs – Criação de API para o aplicativo TinDev</h3>
+          <p>08/02/20 – 6h.</p>
+        </li>
+
+        <li>
+          <h3>Curso de JavaScript e ECMAScript pelo Curso em Vídeo</h3>
+          <p>20/01/20 à 07/02/20 – 10h.</p>
+        </li>
+
+        <li>
+          <h3>10ª Semana OmniStack pela Rocketseat – Criação da aplicação “DevRadar”</h3>
+          <p>13/01/20 à 17/01/20 – 6h.</p>
+        </li>
+
+        <li>
+          <h3>Apresentação do trabalho de conclusão de curso “Modelagem 3D do Zoológico de Bauru” no evento XXVI FÓRUM DE INICIAÇÃO CIENTÍFICA – PIBIC; VII FÓRUM DE DESENVOLVIMENTO TECNOLÓGICO E INOVAÇÃO – PIBITI; VI FÓRUM DE INICIAÇÃO CIENTÍFICA DO ENSINO MÉDIO - PIBIC-EM</h3>
+          <p>04/11/19 à 08/11/19 – 3h.</p>
+        </li>
+
+        <li>
+          <h3>8ª Jornada de Informática da USC – Universidade do Sagrado Coração: Indústria 4.0</h3>
+          <p>10/05/18 à 11/05/18 – 6h30.</p>
+        </li>
+
+        <li>
+          <h3>Plágio na Comunicação em Ambiente Digital - USC</h3>
+          <p>09/04/18 – 3h.</p>
+        </li>
+
+        <li>
+          <h3>Curso de HTML5 e CSS3 pelo SENAC Bauru</h3>
+          <p>17/01/18 à 12/03/18 – 40 h.</p>
+        </li>
+
+        <li>
+          <h3>7ª Jornada de Informática da USC – Universidade do Sagrado Coração: Games e Realidade Virtual</h3>
+          <p>11/05/17 à 12/05/17 - 14 h.</p>
+        </li>
+
+        <li>
+          <h3>Programa de Aprimoramento 2016 - Língua Portuguesa no cotidiano: questões de escrita (Online) - USC</h3>
+          <p>17/08/16 à 14/12/16 - 30 h.</p>
+        </li>
+
+        <li>
+          <h3>6ª Jornada de Informática da USC – Universidade do Sagrado Coração: Internet das Coisas</h3>
+          <p>02/05/16 à 03/05/16 - 6 h.</p>
+        </li>
+
+        <li>
+          <h3>Programa de Aprimoramento 2016 - Conceitos Básicos em Matemática (Online) - USC</h3>
+          <p>16/02/16 à 20/06/16 - 30 h.</p>
+        </li>
+      </ul>
+    </StylesQualificationsOthers>
+  )
+}
+
+export default ContainerQualificationsOthers
