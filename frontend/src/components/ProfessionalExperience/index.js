ffdc98b3b6800060c308f09873d98230f8414842
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-08-27 00:14:35 -0300

    Creation of the Enterprise component (abstraction to make it more generic)

--- frontend/src/components/ProfessionalExperience/index.js
+++ frontend/src/components/ProfessionalExperience/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { StylesProfessionalExperience } from './styles';
 
+import Enterprise from './Components/Enterprise';
+
 import abc71Logo from '../../assets/abc71-logo.png';
 import speksciencesLogo from '../../assets/speksciences-logo.jpg';
 import tvUnespLogo from '../../assets/tvunesp-logo.png';
@@ -10,56 +12,35 @@ const ContainerProfessionalExperience = () => (
   <StylesProfessionalExperience>
     <h2>Experiência Profissional</h2>
 
-    <div className="abc71 item">
-      <img src={abc71Logo} alt="ABC71" />
-      <div className="information">
-        <h3>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.abc71.com.br/"
-          >
-            ABC71 Sistemas de Gestão para Indústrias
-          </a>
-        </h3>
-        <p className="position">Estágio - Desenvolvedor C++</p>
-        <p className="date">05/11/18 · 15/12/19</p>
-      </div>
-    </div>
+    <Enterprise
+      enterpriseClassName="abc71"
+      logo={abc71Logo}
+      alt="ABC71"
+      url="https://www.abc71.com.br/"
+      enterpriseName="ABC71 Sistemas de Gestão para Indústrias"
+      positionName="Estágio - Desenvolvedor C++"
+      date="05/11/18 · 15/12/19"
+    />
 
-    <div className="speksciences item">
-      <img src={speksciencesLogo} alt="SpekSciences" />
-      <div className="information">
-        <h3>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/company/speksciences/"
-          >
-            Speksciences
-          </a>
-        </h3>
-        <p className="position">Estágio - Desenvolvedor web</p>
-        <p className="date">01/04/19 · 31/05/19</p>
-      </div>
-    </div>
+    <Enterprise
+      enterpriseClassName="speksciences"
+      logo={speksciencesLogo}
+      alt="SpekSciences"
+      url="https://www.linkedin.com/company/speksciences/"
+      enterpriseName="Speksciences"
+      positionName="Estágio - Desenvolvedor web"
+      date="01/04/19 · 31/05/19"
+    />
 
-    <div className="tv-unesp item">
-      <img src={tvUnespLogo} alt="TV Unesp" />
-      <div className="information">
-        <h3>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://tv.unesp.br/"
-          >
-            TV UNESP
-          </a>
-        </h3>
-        <p className="position">Estágio - Suporte técnico</p>
-        <p className="date">10/09/18 · 17/10/18</p>
-      </div>
-    </div>
+    <Enterprise
+      enterpriseClassName="tv-unesp"
+      logo={tvUnespLogo}
+      alt="TV Unesp"
+      url="https://tv.unesp.br/"
+      enterpriseName="Televisão Universitária UNESP"
+      positionName="Estágio - Suporte técnico"
+      date="10/09/18 · 17/10/18"
+    />
   </StylesProfessionalExperience>
 );
 
