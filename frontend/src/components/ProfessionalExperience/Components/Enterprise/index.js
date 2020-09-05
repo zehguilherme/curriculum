34a05f17e14c018eb56fa43c6bc0729c05dab967
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-09-05 12:42:32 -0300

    Add button to show or hide more informations from every enterprise

--- frontend/src/components/ProfessionalExperience/Components/Enterprise/index.js
+++ frontend/src/components/ProfessionalExperience/Components/Enterprise/index.js
@@ -1,28 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+import { FaChevronDown } from 'react-icons/fa';
 
 import { EnterpriseContainer } from './styles';
 
 function Enterprise ({
-  enterpriseClassName, logo, alt, url, enterpriseName, positionName, date,
+  enterpriseClassName, logo, alt, url, enterpriseName, positionName, date, moreInformation1, item1, item2, item3, item4, item5,
 }) {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  function handleShowMoreInformationFromEveryEnterprise () {
+    if (!isButtonClicked) setIsButtonClicked(true);
+    if (isButtonClicked) setIsButtonClicked(false);
+  }
+
   return (
-    <EnterpriseContainer>
-      <img src={logo} alt={alt} className={enterpriseClassName} />
-      <div className="information">
-        <h3>
-          <a
-            title="Site da empresa"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={url}
-          >
-            {enterpriseName}
-          </a>
-        </h3>
-        <p className="position">{positionName}</p>
-        <p className="date">{date}</p>
+    <EnterpriseContainer
+      style={isButtonClicked ? { height: '%' } : null}
+    >
+      <div
+        className="enterprise"
+        style={
+          moreInformation1 || item1 || item2 || item3 || item4 || item5 ? { marginBottom: 20 } : null
+        }
+      >
+        <img
+          src={logo}
+          alt={alt}
+          className={enterpriseClassName}
+        />
+        <div className="information">
+          <h3>
+            <a
+              title="Site da empresa"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={url}
+            >
+              {enterpriseName}
+            </a>
+          </h3>
+          <p className="position">{positionName}</p>
+          <p className="date">{date}</p>
+        </div>
       </div>
+
+      {
+        (moreInformation1 || item1 || item2 || item3 || item4 || item5)
+        && (
+          <div
+            className="more-information-button"
+            style={
+              isButtonClicked && moreInformation1 ? { marginBottom: 20 } : null
+            }
+          >
+            <hr className="divider" />
+
+            <FaChevronDown
+              style={
+                isButtonClicked ? { transition: 'transform 0.3s', transform: 'rotate(180deg)' } : { transition: 'transform 0.3s', transform: 'rotate(0deg)' }
+              }
+
+              className="icon"
+              onClick={handleShowMoreInformationFromEveryEnterprise}
+            />
+
+            <hr className="divider" />
+          </div>
+        )
+      }
+
+      {
+        isButtonClicked
+        && (
+          <div className="more-information">
+            <p>
+              {isButtonClicked && moreInformation1}
+            </p>
+
+            <ul>
+              {
+                (isButtonClicked && item1)
+                  ? (
+                    <li>
+                      {item1}
+                    </li>
+                  ) : ''
+              }
+              {
+                (isButtonClicked && item2)
+                  ? (
+                    <li>
+                      {item2}
+                    </li>
+                  ) : ''
+              }
+              {
+                (isButtonClicked && item3)
+                  ? (
+                    <li>
+                      {item3}
+                    </li>
+                  ) : ''
+              }
+              {
+                (isButtonClicked && item4)
+                  ? (
+                    <li>
+                      {item4}
+                    </li>
+                  ) : ''
+              }
+              {
+                (isButtonClicked && item5)
+                  ? (
+                    <li>
+                      {item5}
+                    </li>
+                  ) : ''
+              }
+            </ul>
+          </div>
+        )
+      }
     </EnterpriseContainer>
   );
 }
@@ -34,7 +135,13 @@ Enterprise.propTypes = {
   , url: PropTypes.string.isRequired
   , enterpriseName: PropTypes.string.isRequired
   , positionName: PropTypes.string.isRequired
-  , date: PropTypes.string.isRequired,
+  , date: PropTypes.string.isRequired
+  , moreInformation1: PropTypes.string
+  , item1: PropTypes.string
+  , item2: PropTypes.string
+  , item3: PropTypes.string
+  , item4: PropTypes.string
+  , item5: PropTypes.string,
 };
 
 export default Enterprise;
