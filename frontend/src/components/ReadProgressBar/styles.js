d06d387e4dc5b95be4ea401df2ccd2d22b0cd114
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-09-27 13:30:58 -0300

    Update Container name; Add .attrs method for the width dynamic style

--- frontend/src/components/ReadProgressBar/styles.js
+++ frontend/src/components/ReadProgressBar/styles.js
@@ -1,11 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const ReadProgressBarContainer = styled.div.attrs(({ width }) => ({
+  style: {
+    width: width || '0px',
+  },
+}))`
   height: 5px;
-  width: ${(props) => props.width || '0px'};
-  background-color: #42566a;
   position: fixed;
+  background-color: #42566a;
   left: 0;
   top: 0;
   z-index: 9999;
